3e59de012e6e53fa391c18be99cdb2a4c3429766
EKostinoudis <EKostinoudis@protonmail.com>
2023-04-25 22:10:26 +0300

    initial commit

---
+++ model.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SRCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # first and last channels are 3 because we have RGB images
+        # channels = [3, 64, 32, 3]
+        # self.layers = nn.ModuleList([nn.Conv2d(channels[i], channels[i+1], kernel_size=3, padding=1) for i in range(len(channels)-1)])
+        self.model = nn.Sequential(
+                        nn.Conv2d(3, 64, kernel_size=3, padding=1), 
+                        nn.ReLU(),
+                        nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2), 
+                        nn.ReLU(),
+                        nn.Conv2d(32, 3, kernel_size=3, padding=1)
+                        )
+
+        for l in self.model[:-1]:
+            if isinstance(l, nn.Conv2d) or isinstance(l, nn.ConvTranspose2d):
+                nn.init.kaiming_normal_(l.weight.data, mode='fan_out', nonlinearity='relu')
+                nn.init.zeros_(l.bias.data)
+        # last layer
+        # nn.init.normal_(self.model[-1].weight.data, 0.0, 0.001)
+        # nn.init.normal_(self.model[-1].weight.data, 0.0, 0.01)
+        # nn.init.zeros_(self.model[-1].bias.data)
+    
+    def forward(self, x):
+        '''
+        for layer in self.layers[:-1]:
+            x = F.relu(layer(x))
+        return self.layers[-1](x)
+        '''
+        return self.model(x)
+
+class SRCNN2(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # first and last channels are 3 because we have RGB images
+        self.model = nn.Sequential(
+                        nn.Conv2d(3, 128, kernel_size=3, padding=1), 
+                        nn.ReLU(),
+                        nn.ConvTranspose2d(128, 32, kernel_size=2, stride=2), 
+                        nn.ReLU(),
+                        nn.Conv2d(128, 64, kernel_size=3, padding=1), 
+                        nn.ReLU(),
+                        nn.Conv2d(32, 3, kernel_size=3, padding=1)
+                        )
+
+        for l in self.model[:-1]:
+            if isinstance(l, nn.Conv2d) or isinstance(l, nn.ConvTranspose2d):
+                nn.init.kaiming_normal_(l.weight.data, mode='fan_out', nonlinearity='relu')
+                nn.init.zeros_(l.bias.data)
+    
+    def forward(self, x):
+        return self.model(x)
---
+++ utils.py
@@ -0,0 +1,16 @@
+import torch
+import os
+import matplotlib.pyplot as plt
+
+def save_model(model, path, name):
+    os.makedirs(path, exist_ok = True) 
+    torch.save(model, os.path.join(path, name))
+
+def save_plot(fig, name, folder=None):
+    path = "figures"
+    if folder is not None:
+        path = os.path.join(path, folder)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+    fig.savefig(os.path.join(path, f"{name}.png"), bbox_inches='tight')
